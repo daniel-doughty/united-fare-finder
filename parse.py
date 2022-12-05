7b9d21f96b79c236af289fd344559ff68ac7a6b3
cdc-001 <cory.caddell@gmail.com>
2022-12-05 14:30:34 -0600

    Adjust to adhere to style guide

--- parse.py
+++ parse.py
@@ -1,34 +1,48 @@
-#parse.py
+# parse.py
 
 from bs4 import BeautifulSoup
-import requests
-import sys
 
-#function to parse page source raw data
+
 def parsePageSource(page_source):
-    
-    parseFile = "output.txt"                                                                                                #File storing parsed data
+    # function to parse page source raw data
+
     soup = BeautifulSoup(page_source, "html.parser")
 
-    #read input and save to list
-    flight_rows = soup.find_all("div", class_ = "app-components-Shopping-GridItem-styles__flightRow--1E4Sk")                #Flight rows per website
+    # flight listings per website layout (from top to bottom)
+    flight_rows = soup.find_all(
+        "div",
+        class_="app-components-Shopping-GridItem-styles__flightRow--1E4Sk")
     rows = (len(flight_rows))
 
-    flight_elements = [[[] for _ in range(7)] for _ in range(rows)]                                                         #List to hold flight information per flight
+    # list to hold flight information for each flight
+    flight_elements = [[[] for _ in range(7)] for _ in range(rows)]
 
     rowIndex = 0
     for flight_rows in flight_rows:
-        flight_info = flight_rows.find_all("span", class_ = "app-components-AriaMessage-styles__visuallyHidden--2RXE0")     #Same class is used for almost all relevant text
+
+        # same class is used for almost all relevant text
+        flight_info = flight_rows.find_all(
+            "span",
+            class_="app-components-AriaMessage-styles__visuallyHidden--2RXE0")
+
         colIndex = 0
         for flight_info in flight_info:
-            flight_elements[rowIndex][colIndex].append(flight_info.text.strip())
+            flight_elements[rowIndex][colIndex].append(
+                flight_info.text.strip())
             colIndex += 1
-        flight_elements[rowIndex][colIndex].append(flight_rows.find("div", class_ = "app-components-Shopping-PriceCard-styles__top--2IU8G").get_text()) #Get miles and cost
+
+        # miles and cost uses a different class
+        flight_cost = flight_rows.find(
+            "div",
+            class_="app-components-Shopping-PriceCard-styles__top--2IU8G")
+        flight_elements[rowIndex][colIndex].append(
+            flight_cost.text.strip())
         rowIndex += 1
 
-    #write to file
-    with open(parseFile, "w") as f:                    
+    # write to file
+    parseFile = "output.txt"
+    with open(parseFile, "w") as f:
         for i in range(len(flight_elements)):
             for j in range(len(flight_elements[i])):
-                print(*flight_elements[i][j], file = f)
-            print("", file = f)
+                print(*flight_elements[i][j], file=f)
+            print("", file=f)
--- united-fare-finder.py
+++ united-fare-finder.py
@@ -2,25 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from parse import *                                     #Function to parse raw data
+from parse import parsePageSource
 import time
 import config
-
 
 
 mileage_plus_number = config.mileage_plus_number
 password = config.password
 cookies = config.cookies
 
-driver = webdriver.Chrome()                             
+driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get("https://www.united.com")
 
-driver.delete_all_cookies()                 
-for cookie in cookies:                      
-  driver.add_cookie(cookie)
+driver.delete_all_cookies()
+for cookie in cookies:
+    driver.add_cookie(cookie)
 
-assert "United Airlines" in driver.title    
+assert "United Airlines" in driver.title
 
 elem = driver.find_element(By.ID, "loginButton")
 elem.click()
@@ -57,22 +56,18 @@ elem.clear()
 elem.send_keys("03/10/23")
 elem.send_keys(Keys.TAB)
 
-elem = driver.find_element(By.XPATH, '//*[@id="bookFlightForm"]/div[1]/div[1]/label')
+elem = driver.find_element(
+  By.XPATH, '//*[@id="bookFlightForm"]/div[1]/div[1]/label')
 elem.click()
 
-elem = driver.find_element(By.CSS_SELECTOR, "button.app-components-BookFlightForm-bookFlightForm__findFlightBtn--1lbFe")
+elem = driver.find_element(
+  By.CSS_SELECTOR,
+  "button.app-components-BookFlightForm-bookFlightForm__findFlightBtn--1lbFe")
 elem.submit()
 
-time.sleep(10)
-  
+time.sleep(10)\
+
 page_source = driver.page_source
 parsePageSource(page_source)
 
 driver.close()
-
-
-
-
-
-
-
