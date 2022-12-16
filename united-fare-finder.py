27e6308191c6b326ca0e9f119668d25f1ceaafff
cdc-001 <cory.caddell@gmail.com>
2022-12-16 10:50:54 -0600

    add flight iterations

--- united-fare-finder.py
+++ united-fare-finder.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 from parse import parsePageSource
 import time
 import config
-
 
 mileage_plus_number = config.mileage_plus_number
 password = config.password
@@ -36,45 +36,96 @@ elem.send_keys(Keys.RETURN)
 elem = driver.find_element(By.ID, "closeBtn")
 elem.click()
 
-elem = driver.find_element(By.ID, "bookFlightOriginInput")
-elem.clear()
-elem.send_keys("ORD")
-elem.send_keys(Keys.TAB)
+origin = "ORD"
+dest = [
+  "hnl", "slc", "dfw", "lax", "sea", "atl",
+  "mia", "mco", "sfo", "jfk", "ewr", "lga", "dca", "iad"
+]
+depDate = "03/01/2023"
+retDate = "03/10/2023"
 
-elem = driver.find_element(By.ID, "bookFlightDestinationInput")
-elem.clear()
-elem.send_keys("HNL")
-elem.send_keys(Keys.TAB)
+count = 0
+for dest in dest:
 
-elem = driver.find_element(By.ID, "DepartDate")
-elem.clear()
-elem.send_keys("03/03/23")
-elem.send_keys(Keys.TAB)
+    # input flight info in main page
+    if (count == 0):
+        elem = driver.find_element(By.ID, "bookFlightOriginInput")
+        elem.clear()
+        elem.send_keys(origin)
 
-elem = driver.find_element(By.ID, "ReturnDate")
-elem.clear()
-elem.send_keys("03/10/23")
-elem.send_keys(Keys.TAB)
+        elem = driver.find_element(By.ID, "bookFlightDestinationInput")
+        elem.clear()
+        elem.send_keys(dest)
 
-elem = driver.find_element(
-  By.XPATH, '//*[@id="bookFlightForm"]/div[1]/div[1]/label')
-elem.click()
+        elem = driver.find_element(By.ID, "DepartDate")
+        elem.clear()
+        elem.send_keys(depDate)
 
-elem = driver.find_element(
-  By.CSS_SELECTOR,
-  "button.app-components-BookFlightForm-bookFlightForm__findFlightBtn--1lbFe")
-elem.submit()
+        elem = driver.find_element(By.ID, "ReturnDate")
+        elem.clear()
+        elem.send_keys(retDate)
 
-time.sleep(10)
+        elem = driver.find_element(
+          By.XPATH, '//*[@id="bookFlightForm"]/div[1]/div[1]/label')
+        elem.click()
 
-# scrape fare data
-page_source = driver.page_source
-flight_elements = parsePageSource(page_source)
+        elem = driver.find_element(
+          By.CSS_SELECTOR,
+          "button."
+          "app-components-BookFlightForm-bookFlightForm__findFlightBtn--1lbFe")
+        elem.submit()
 
-# write to file
-for i in range(len(flight_elements)):
-    for j in range(len(flight_elements[i])):
-        print(*flight_elements[i][j])
-    print()
+    # input flight info in subsequent pages
+    else:
+        elem = driver.find_element(
+          By.ID, "airport-autocomplete-.swap0.fromTrip")
+        elem.send_keys(Keys.CONTROL + "a")
+        elem.send_keys(origin)
+
+        elem = driver.find_element(
+          By.XPATH,
+          '//li[@id="airport-autocomplete-.swap0.fromTrip-menu-item-0"]//div')
+        elem.click()
+
+        elem = driver.find_element(By.ID, "airport-autocomplete-.swap0.toTrip")
+        elem.send_keys(Keys.CONTROL + "a")
+        elem.send_keys(dest)
+
+        elem = driver.find_element(
+          By.XPATH,
+          '//li[@id="airport-autocomplete-.swap0.toTrip-menu-item-0"]//div')
+        elem.click()
+
+        elemList = driver.find_elements(
+          By.XPATH, '//div[@class="DateInput DateInput_1"]/input')
+
+        elem = elemList[0]
+        elem.send_keys(Keys.CONTROL + "a")
+        elem.send_keys(depDate)
+
+        elem = elemList[1]
+        elem.send_keys(Keys.CONTROL + "a")
+        elem.send_keys(retDate)
+
+        elem = driver.find_element(
+          By.CSS_SELECTOR,
+          ".atm-c-btn.atm-c-btn--secondary")
+        elem.click()
+
+    time.sleep(15)
+
+    # scrape fare data
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, "html.parser")
+    flight_elements = parsePageSource(soup)
+
+    # write to file
+    print(dest + " ********************************************************\n")
+    for i in range(len(flight_elements)):
+        for j in range(len(flight_elements[i])):
+            print(*flight_elements[i][j])
+        print()
+
+    count = 1
 
 driver.close()
